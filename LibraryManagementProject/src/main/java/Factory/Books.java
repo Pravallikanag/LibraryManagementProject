4cf3f3717ce2140733e68410c0b60fb822141ef2
Pravallikanag <pravallikanagalaku@lewisu.edu>
2025-06-16 21:17:17 -0500

    Add files via upload
    This is where you give context, especially if someone else views your repo. For example:
    Uploaded compiled `.class` files for Books, Loans, and Members, as well as Maven-generated files from the target directory. These are part of the build output for the LibraryManagement project.

---
+++ LibraryManagementProject/src/main/java/Factory/Books.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Factory;
+public class Books {
+	private int bookID;
+	private String author, title, category;
+	// constructor to create a user
+	Books(){
+		bookID = 0;
+	}
+	// parameter constructor to simplify the creation of book objects
+	Books(int id, String author, String title, String category){
+		setBookID(id);
+		setAuthor(author);
+		setTitle(title);
+		setCategory(category);
+	}
+	// mutator methods
+
+	// set book id increasing order
+	void setBookID(int id) {
+		bookID = id + 1;
+	}
+	// set book last name
+	void setAuthor(String author) {
+		this.author = author;
+	}
+	// set book last name
+	void setTitle(String title) {
+		this.title = title;
+	}
+	// set book phone number
+	void setCategory(String category) {
+		this.category = category;
+	}
+	
+	// accessor methods
+	
+	// get book id
+	int getBookID() {
+		return bookID;
+	}
+	// return author
+	String getAuthor() {
+		return author;
+	}
+	// return title
+	String getTitle() {
+		return title;
+	}
+	// return title
+	String getCategory() {
+		return category;
+	}
+	
+	// print book details
+	
+	public String toString() {
+		return ("Book ID: " + bookID + " | Book Author: " + author + " | Book Title: " + title + " | Category: " + category);
+	}
+
+}
+
